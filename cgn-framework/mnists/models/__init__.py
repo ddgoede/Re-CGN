08a6234207dd0f53c498b908dbe56c31b93cb0b2
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2022-01-14 18:07:18 +0100

    WIP: Adds support for Generator and GAN training

--- cgn-framework/mnists/models/__init__.py
+++ cgn-framework/mnists/models/__init__.py
@@ -1,7 +1,8 @@
 from mnists.models.cgn import CGN
 from mnists.models.discriminator import DiscLin, DiscConv
 from mnists.models.classifier import CNN
+from mnists.models.generator import Generator
 
 __all__ = [
-    CGN, DiscLin, DiscConv, CNN
+    CGN, DiscLin, DiscConv, CNN, Generator,
 ]
