6afe094052f4f6543cdcc8bb48e06f07044f1c07
Danilo <danilo@omen.home>
2022-01-15 14:12:22 +0100

    set_env.py

---
+++ experiments/set_env.py
@@ -0,0 +1,11 @@
+import os, sys
+from os.path import join, dirname, abspath
+
+
+def set_env():
+    """This function does two things:
+    1. It allows the code in cgn_framework to be imported from the experiments folder.
+    2. It changes the current working directory to cgn_framework/. This is necessary,
+    because the code present in this directory expects to be called from here."""
+    sys.path.insert(0, dirname(dirname(abspath(__file__))))
+    os.chdir(join(dirname(dirname(abspath(__file__))), "cgn_framework"))
