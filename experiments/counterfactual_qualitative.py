ae61960badf9fec09370810909e32c21810fd2c2
Danilo <danilo@omen.home>
2022-01-15 14:16:03 +0100

    Improved counterfactual_qualitative and added results for the real images of Figure 3

--- experiments/counterfactual_qualitative.py
+++ experiments/counterfactual_qualitative.py
@@ -14,38 +14,57 @@ from torchvision.utils import make_grid
 from matplotlib import pyplot as plt
 import os
 
+from set_env import set_env
+set_env()
+
+from cgn_framework.mnists.generate_data import generate_cf_dataset, generate_dataset, get_dataloaders
+from cgn_framework.mnists.train_cgn import CGN
 
 DATASETS = {"colored_MNIST": "cgn_colored_MNIST",
             "double_colored_MNIST": "cgn_double_colored_MNIST",
             "wildlife_MNIST": "cgn_wildlife_MNIST"
             }
 
-
 def generate_counterfactual_images(dataset_size=100000, no_cfs=10):
     """Generate the counterfactual images for the 3 datasets."""
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     for dataset, weight_folder in DATASETS.items():
-        generated_images_path = f"./cgn_framework/mnists/data/{dataset}_counterfactual.pth"
+        # First create the real images.
+        dl_train, dl_test = get_dataloaders(dataset, batch_size=1000, workers=8)
 
-        if os.path.exists(generated_images_path):
-            print(f"Counterfactual images for {dataset} already exist, skipping..")
-            continue
+        if os.path.exists(f"mnists/data/{dataset}_train.pth"):
+            print(f"'{dataset}_train.pth' already exist, skipping..")
+        else:
+            generate_dataset(dl=dl_train, path=dataset + '_train.pth')
 
-        command = f"""python ./mnists/generate_data.py \
-                        --weight_path ./mnists/experiments/{weight_folder}/weights/ckp.pth \
-                        --dataset {dataset} --no_cfs {no_cfs} --dataset_size {dataset_size}"""
+        if os.path.exists(f"mnists/data/{dataset}_test.pth"):
+            print(f"'{dataset}_test.pth' already exist, skipping..")
+        else:
+            generate_dataset(dl=dl_test, path=dataset + '_test.pth')
 
-        working_directory = os.getcwd()
-        os.chdir(f"{working_directory}/cgn_framework")
-        print(os.system(command))
-        os.chdir(working_directory)
+        # Next, generate the counterfactual images.
+        weight_path = f"mnists/experiments/{weight_folder}/weights/ckp.pth"
+
+        if os.path.exists(f"mnists/data/{dataset}_counterfactual.pth"):
+            print(f"'{dataset}_counterfactual.pth' already exist, skipping..")
+        else:
+            cgn = CGN()
+            cgn.load_state_dict(torch.load(weight_path, 'cpu'))
+            cgn.to(device).eval()
+            print(f"Generating the counterfactual {dataset} of size {dataset_size}")
+            generate_cf_dataset(cgn=cgn, path=dataset + '_counterfactual.pth',
+                                dataset_size=dataset_size, no_cfs=no_cfs,
+                                device=device)
 
 
 def main():
     generate_counterfactual_images()
 
+    # Plot the real and counterfactual iamges (i.e., Figure 3 of the paper).
     for dataset, weight_folder in DATASETS.items():
-        generated_images_path = f"./cgn_framework/mnists/data/{dataset}_counterfactual.pth"
-        visualise_generated_images(generated_images_path)
+        visualise_generated_images(f"mnists/data/{dataset}_train.pth")
+        visualise_generated_images(f"mnists/data/{dataset}_counterfactual.pth")
 
 def visualise_generated_images(path):
     images, labels = torch.load(path)
