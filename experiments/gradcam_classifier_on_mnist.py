59c09bfa5711fbd63f8399135f3f1776c458ec95
Piyush <piyushnbagad11@gmail.com>
2022-01-17 18:16:07 +0100

    Minor cleanup for gradcam script]

--- experiments/gradcam_classifier_on_mnist.py
+++ experiments/gradcam_classifier_on_mnist.py
@@ -6,14 +6,12 @@ import numpy as np
 import torch
 from torchvision import transforms
 from torchvision.utils import save_image
+from tqdm import tqdm
+from gradcam.utils import visualize_cam
+from gradcam import GradCAM, GradCAMpp
 
 import warnings
 warnings.filterwarnings("ignore")
-
-# from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
-# from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-# from pytorch_grad_cam.utils.image import show_cam_on_image
-
 
 from experiment_utils import set_env, REPO_PATH
 set_env()
@@ -21,16 +19,6 @@ set_env()
 from cgn_framework.mnists.models.classifier import CNN
 from cgn_framework.mnists.train_cgn import save
 from cgn_framework.mnists.dataloader import get_tensor_dataloaders, TENSOR_DATASETS
-
-def show(imgs):
-    if not isinstance(imgs, list):
-        imgs = [imgs]
-    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
-    for i, img in enumerate(imgs):
-        img = img.detach()
-        img = F.to_pil_image(img)
-        axs[0, i].imshow(np.asarray(img))
-        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
 
 def main(args):
@@ -71,46 +59,12 @@ def main(args):
     # apply GradCAM on the test set
     print("Applying GradCAM on the test set")
 
-    ############################### TRY 1 ###############################
-    # target_layers = [model.cls[0]]
-    # input_tensor, input_label = data[0], target[0]
-
-    # # Construct the CAM object once, and then re-use it on many images:
-    # cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
-
-    # # You can also use it within a with statement, to make sure it is freed,
-    # # In case you need to re-create it inside an outer loop:
-    # # with GradCAM(model=model, target_layers=target_layers, use_cuda=args.use_cuda) as cam:
-    # #   ...
-
-    # # We have to specify the target we want to generate
-    # # the Class Activation Maps for.
-    # # If targets is None, the highest scoring category
-    # # will be used for every image in the batch.
-    # # Here we use ClassifierOutputTarget, but you can define your own custom targets
-    # # That are, for example, combinations of categories, or specific outputs in a non standard model.
-    # # targets = [e.g ClassifierOutputTarget(281)]
-
-    # # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
-    # grayscale_cam = cam(input_tensor=input_tensor.unsqueeze(0))
-    # import ipdb; ipdb.set_trace()
-
-    # # In this example grayscale_cam has only one image in the batch:
-    # grayscale_cam = grayscale_cam[0, :]
-    # visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
-    ################################################################################
-
-    ############################### TRY 2 ###############################
-    from gradcam.utils import visualize_cam
-    from gradcam import GradCAM, GradCAMpp
-
-    model = model.train()
-
+    # define the target layer to be used
     target_layer = model.model[8]
     gradcam = GradCAM(model, target_layer)
 
-    # results = []
-    for i in range(len(images)):
+    # loop over images
+    for i in tqdm(range(len(images)), desc="Applying GradCAM on one image at a time"):
         image = images[i]
         mask, _ = gradcam(image.unsqueeze(0))
         heatmap, result = visualize_cam(mask, image)
@@ -118,11 +72,6 @@ def main(args):
         # save result for this image
         path = save_path.replace("test_samples", f"test_sample_{i}_gradcam")
         save_image([image.data, heatmap, result], path, nrow=3, padding=0, normalize=True)
-
-        # results.extend([image.cpu(), heatmap, result])
-    # grid_image = make_grid(results, nrow=len(images))
-    #View results
-    # x = transforms.ToPILImage()(grid_image)
     
 
 
