edc87d1cf7123be5074d8e3ea3b5e888c81c373e
Piyush <piyushnbagad11@gmail.com>
2022-01-25 22:12:16 +0100

    Minor

--- experiments/download_imagenet_sketch.sh
+++ experiments/download_imagenet_sketch.sh
@@ -5,5 +5,5 @@ echo "Downloading ImageNet-Sketch at "$REPO_PATH/cgn_framework/imagenet/data/ske
 gdown "https://drive.google.com/uc?id=1Mj0i5HBthqH1p_yeXzsg22gZduvgoNeA" -O $REPO_PATH/cgn_framework/imagenet/data/
 unzip $REPO_PATH/cgn_framework/imagenet/data/ImageNet-Sketch.zip -d $REPO_PATH/cgn_framework/imagenet/data/
 rm -rf $REPO_PATH/cgn_framework/imagenet/data/ImageNet-Sketch.zip
-mkdir -p $REPO_PATH/cgn_framework/imagenet/data/sketch/val/
-mv $REPO_PATH/cgn_framework/imagenet/data/sketch/* $REPO_PATH/cgn_framework/imagenet/data/sketch/val/
+mkdir -p $REPO_PATH/cgn_framework/imagenet/data/in-sketch/val/
+mv $REPO_PATH/cgn_framework/imagenet/data/in-sketch/* $REPO_PATH/cgn_framework/imagenet/data/in-sketch/val/
