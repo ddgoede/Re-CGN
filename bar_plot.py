f2f21d1b99525c8382a2f0e14b4ee7d2b0c706a6
aeduin@nanquanu.org <aeduin@nanquanu.org>
2022-01-27 16:30:05 +0100

    bar plot style

--- bar_plot.py
+++ bar_plot.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from turtle import color
 from matplotlib import pyplot as plt
 from matplotlib.axis import XAxis
 import numpy as np
@@ -44,8 +45,8 @@ fixed_sum = sum(data_fixed)
 
 x_axis = np.arange(0, 11)
 
-plt.bar(x_axis - 0.2, [d / unfixed_sum for d in data_unfixed], width=0.4, label='no center loss')
-plt.bar(x_axis + 0.2, [d / fixed_sum for d in data_fixed], width=0.4, label='with center loss')
+plt.bar(x_axis - 0.2, [d / unfixed_sum for d in data_unfixed], width=0.4, label='No center loss', color="skyblue")
+plt.bar(x_axis + 0.2, [d / fixed_sum for d in data_fixed], width=0.4, label='With center loss', color="pink")
 plt.xticks(x_axis)
 plt.ylabel('fraction of runs')
 plt.xlabel('number of not-broken masks')
