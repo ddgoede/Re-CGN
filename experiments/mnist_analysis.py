99fefdb2d397cdcdf558f15c4581e32c79f11904
Piyush <piyushnbagad11@gmail.com>
2022-02-01 02:16:11 +0100

    Done with testing the entire final demo notebook

--- experiments/mnist_analysis.py
+++ experiments/mnist_analysis.py
@@ -357,7 +357,8 @@ def run_analyses(seed=0, datasets=["colored_MNIST"], show=False, debug=False, ig
             show_gradcam_qualitative(dataset, images_paths_og, images_paths_cf)
 
     # if show==True, display all figures/results together
-    plot_iou_for_og_and_cf(iou_path_og_per_dataset, iou_path_cf_per_dataset, show=show)
+    if show:
+        plot_iou_for_og_and_cf(iou_path_og_per_dataset, iou_path_cf_per_dataset, show=show)
 
 
 if __name__ == "__main__":
